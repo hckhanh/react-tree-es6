d4821da9ceedea813f7b3efcae766e2bfc91d9f2
Hoang Chau Khanh <Khanh.HoangChau@vn.bosch.com>
2016-08-15 16:10:24 +0700

    Update README file (#20)

--- src/react-tree.js
+++ src/react-tree.js
@@ -27,10 +27,10 @@ ReactTree.propTypes = {
   core: function (props, propName, componentName) {
     const core = props[propName];
     if (!core || typeof (core) !== 'object')
-      return new TypeError(`Invalid prop \`core\`: must follow jsTree API, read more https://www.jstree.com/api/#/?q=core`)
+      return new TypeError(`Invalid prop \`core\`: must follow jsTree API, read more https://www.jstree.com/api/#/?q=core`);
 
     const data = core.data;
     if (!Array.isArray(data) && typeof (data) !== 'object')
-      throw new TypeError(`Invalid object \`core.data\`: must be array of object of jsTree JSON data, read more https://www.jstree.com/docs/json`)
+      throw new TypeError(`Invalid object \`core.data\`: must be array of object of jsTree JSON data, read more https://www.jstree.com/docs/json`);
   }
 };
